df2d04efab39b1e5713c68c5cf26e9a47bd49f09
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-08-04 15:39:42 +0400

    basic useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const state = useState();
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{count}</h1>
+      <div className="container">
+        <button className="button" onClick={() => setCount(count * 91)}>
+          click me
+        </button>
+      </div>
+    </>
   );
-}
-
+};
 export default App;
